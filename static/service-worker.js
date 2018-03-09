4083e89bfcc51dc90d6ec1b1ce350bbb183df12a
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2018-03-09 11:12:45 -0500

    Document service worker file

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,3 +1,5 @@
+// This is based on the First Progressive Web App Tutorial by Google
+// https://codelabs.developers.google.com/codelabs/your-first-pwapp/
 const cacheName = 'flask-PWA-v3';
 const filesToCache = [
     '/',
@@ -7,6 +9,9 @@ const filesToCache = [
     '/offline.html'
 ];
 
+// When the 'install' event is fired we will cache
+// the html, javascript, css, images and any other files important
+// to the operation of the application shell
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
@@ -17,6 +22,9 @@ self.addEventListener('install', function(e) {
   );
 });
 
+// We then listen for the service worker to be activated/started. Once it is
+// ensures that your service worker updates its cache whenever any of the app shell files change.
+// In order for this to work, you'd need to increment the cacheName variable at the top of this service worker file.
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
     e.waitUntil(
@@ -32,6 +40,10 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
+
+// Serve the app shell from the cache
+// If the file is not in the cache then try to get it via the network.
+// otherwise give an error and display an offline page
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
