b464cc91385413acc2cf3e9d0a324cf27248645f
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2018-03-14 16:20:23 -0500

    Add route to demonstrate offline page fallback

--- flask-pwa.py
+++ flask-pwa.py
@@ -7,21 +7,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route('/some-other-page')
+def other_page():
+    return render_template('other-page.html')
+
 
 @app.route('/offline.html')
 def offline():
     return app.send_static_file('offline.html')
 
+
 @app.route('/service-worker.js')
 def sw():
     return app.send_static_file('service-worker.js')
 
-@app.route('/api/news')
-def api_news():
-    return jsonify({'message': 'testing 1 2 3'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
